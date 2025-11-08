575a5cefebab712275c2dab697013aa133a85f08
Manuel Pérez <modnueding@gmail.com>
2025-11-08 18:00:14 +0100

    persistencia

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from task_manager import TaskManager
 def print_menu():
     print("\n--- Gestor de Tareas Inteligente ---")
     print("1. Añadir tarea")
-    print("2. Añadir tarea compleja (con IA)")
-    print("3. Listar tareas")
-    print("4. Completar tarea")
-    print("5. Eliminar tarea")
-    print("6. Salir")    
+    #print("2. Añadir tarea compleja (con IA)")
+    print("2. Listar tareas")
+    print("3. Completar tarea")
+    print("4. Eliminar tarea")
+    print("5. Salir")    
 
 def main():
     manager=TaskManager()
@@ -15,28 +15,31 @@ def main():
     while True:
         print_menu()
 
-        choice =input("Elige una opción")
+        try:
 
-        match choice:
-                case "1":
-                    description=input("Dime descripcion")
-                    manager.add_task(description)
-                    
-                case "2":
-                    manager.list_task()
-                case "3":
-                    id=input("id de la tarea a completar")
-                    manager.complete_task(id)
-                case "4":
-                    id=input("id de la tarea a eliminar")
-                    manager.delete_task(id)
-                case "5":
-                    pass
-                case "6":
-                    print("Saliendo")
-                    break
-                case _:    
-                    print("Opción no ovalidad")
+            choice =int(input("Elige una opción"))
 
+            match choice:
+                    case 1:
+                        description=input("Dime descripcion")
+                        manager.add_task(description)
+                        
+                    case 2:
+                        manager.list_task()
+                    case 3:
+                        id=int(input("id de la tarea a completar"))
+                        manager.complete_task(id)
+                    case 4:
+                        id=int(input("id de la tarea a eliminar"))
+                        manager.delete_task(id)
+                    case 5:
+                        pass
+                    case 6:
+                        print("Saliendo")
+                        break
+                    case _:    
+                        print("Opción no valida")
+        except ValueError:
+            print("Opción no valida")
 if __name__ == "__main__":#nombre del fichero = a main
     main()
--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,4 @@
+import json
 class Task:
     def __init__(self, id, description, completed=False):
         self.id=id
@@ -9,15 +10,20 @@ class Task:
         return f"[{status}] #{self.id}: {self.description}"
 
 class TaskManager:
+
+    FILENAME="task.json"
+
     def __init__(self):
         self._tasks=[]
         self._next_id=1
+        self.load_tasks()
 
     def add_task(self, description):
         task=Task(self._next_id, description)
         self._tasks.append(task)
         self._next_id+=1
         print(f"Tarea añadida: {description}")
+        self.save_tasks()
 
     def list_task(self):
         if not self._tasks:
@@ -31,6 +37,7 @@ class TaskManager:
             if task.id == id:
                 task.completed=True
                 print (f"Tarea completada:  {task}")
+                self.save_tasks()
                 return
         print (f"Tarea no encontrada: #{id}")
 
@@ -39,5 +46,23 @@ class TaskManager:
             if task.id == id:
                 self._tasks.remove(task)
                 print (f"Tarea eliminada:  {task}")
+                self.save_tasks()
                 return
         print (f"Tarea no encontrada: #{id}")
+
+    def load_tasks(self):
+        try:
+            with open(self.FILENAME, "r") as file:
+                data = json.load(file)
+                self._tasks = [Task(item["id"], item["description"], item["completed"]) for item in data]
+                if self._tasks:
+                    self._next_id = self._tasks[-1].id + 1
+                else:
+                    self._next_id = 1    
+        except FileNotFoundError:
+            self._tasks = []
+            
+
+    def save_tasks(self):
+        with open(self.FILENAME, "w") as file: 
+            json.dump([{"id":task.id, "description":task.description, "completed":task.completed} for task in self._tasks],file, indent=4)
